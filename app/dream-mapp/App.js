8ecab0c1f9c0b48e32577725c0247030e792d65f
Raul Oliveira <rshinaede@outlook.com>
2023-03-13 18:47:39 -0300

    Configurando expo

---
+++ app/dream-mapp/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+
+  const _goBack = () => console.log('Went back');
+  const _handleSearch = () => console.log('Searching');
+  const _handleMore = () => console.log('Shown more');
+
+  return (
+    <SafeAreaProvider>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={_goBack} />
+        <Appbar.Content title="Title" />
+        <Appbar.Action icon="magnify" onPress={_handleSearch} />
+        <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+      </Appbar.Header>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
